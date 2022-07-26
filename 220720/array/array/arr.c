aaf779cc22c9377a5d7fab1c86edf57b46f4c6b7
jayseong120 <sjy@emotiontek.com>
2022-07-26 16:26:02 +0900

    위에 세모 완성
    ~~방식으로 만들었습니다

--- 220720/array/array/arr.c
+++ 220720/array/array/arr.c
@@ -140,29 +140,37 @@
 //		printf("\n");
 //	}
 //}
-//int main()
-//{
-//	int N;
-//	scanf("%d", &N);
-//	for (int i = 1; i <= N; i++)
-//	{
-//		for (int j = 1; j <= N-i; j++)
-//			printf(" ", j);
-//		for (int k = 1; k <= 2*i-1; k++)
-//			printf("*");
-//		printf("\n");
-//	}
-//}
 int main()
 {
 	int N;
 	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
+	for (int i = 1; i <= N; i++)
 	{
-		for (int k = 0; k < i; k++)
-			printf(" ");
-		for (int j = 0; j < 2*N*i-1; j++)
+		for (int j = 1; j <= N-i; j++)
+			printf(" ", j);
+		for (int k = 1; k <= 2*i-1; k++)
+			printf("*");
+		printf("\n");
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N-i; j++)
+			printf(" ", j);
+		for (int k = 1; k <= 2*i-1; k++)
 			printf("*");
 		printf("\n");
 	}
 }
+//int main()
+//{
+//	int N;
+//	scanf("%d", &N);
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int k = 0; k < i; k++)
+//			printf(" ");
+//		for (int j = 0; j < 2*; j++)
+//			printf("*");
+//		printf("\n");
+//	}
+//}
